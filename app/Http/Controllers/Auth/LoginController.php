092151946eace040236cc18db5f69bdf213a6c45
Aya-Farrag <58622631+Aya-Farrag@users.noreply.github.com>
2020-01-25 17:28:57 +0200

    facebook login with socialite and facebok sdk short-term access token
    1-Need changes to convert short term access token to long-lived one which may need to get rid of socialite and implement facebook php sdk login methods.
    
    2-I've add the app id and secret in .env.example so that you could copy it from there.
    
    3- To run the code, you need to make composer require facebook/graph-sdk
    and composer install

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,19 +7,73 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Socialite;
 use App\User;
+use Facebook\Facebook;
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    public function redirectToProviderFacebook()
+    {      
+        //   session_start();
+
+        return Socialite::driver('facebook')->redirect();
+    }
+    public function handleProviderFacebookCallback()
+    {
+        // session_start();
+    
+         $user = Socialite::driver('facebook')->user();
+        // dd($user);
+        $fb=new Facebook(config('facebook.config'));
+        // $response = $fb->get('/me', $user->token);
+    // $access_token='EAACeZCVjM7usBAE7qcz0JqhLFcNeTIYPo1VeX503bzF0IkzZAxVd1sGSEyduvlXq4t3iMfYIGVVRhFPXZBVabQGj4UcAjLcfnpJ1DuNhl1sJJobaHjFeRxHsguN1fnaFv3Ij7M7vaNPQGZB7licNHwRxZAoER6HeRw8uZBVro2zokilaBabul6c1hSBD6lejDkzZA5qo3bWSWi66jZAQaNuy';
+        $fb->setDefaultAccessToken($user->token);
+        $fields = "id,cover,name,first_name,last_name,hometown,age_range,birthday,location,likes,posts,friends,link,gender,email,name_format,locale,picture,timezone,updated_time,verified";
+        $fb_user = $fb->get('/me?fields='.$fields)->getGraphNode();
+        // dd($fb_user);
+        // $accessToken=$user->token;
+        // $helper = $fb->getRedirectLoginHelper();
+        // $accessToken = $helper->getAccessToken();
+        // $oAuth2Client = $fb->getOAuth2Client();
+        // if (! $accessToken->isLongLived()) {
+        //     // Exchanges a short-lived access token for a long-lived one
+        //     try {
+        //       $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+        //     } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        //       echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+        //       exit;
+        //     }
+          
+        //     echo '<h3>Long-lived</h3>';
+        //     var_dump($accessToken->getValue());
+        //   }
+
+        $existingUser = User::where('email', $user->getEmail())->first();
+
+
+        if ($existingUser) {
+            auth()->login($existingUser, true);
+        } else {
+            $newUser                    = new User;
+            $newUser->provider_name     = 'facebook';
+            $newUser->provider_id       = $user->getId();
+            $newUser->name              = $user->getName();
+            $newUser->email             = $user->getEmail();
+            $newUser->email_verified_at = now();
+            $newUser->avatar            = $user->getAvatar();
+            $newUser->country_id        = 1;
+            $newUser->category_id        = 1;
+            $newUser->role              = 'influencer';
+            $newUser->facebook_token    =$user->token;
+            $newUser->save();
+    
+            auth()->login($newUser, true);
+
+        
+    }
+dd($fb_user);
+    }
+    
 
     use AuthenticatesUsers;
 
@@ -38,43 +92,6 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-    public function redirectToProviderFacebook()
-    {
-        return Socialite::driver('facebook')->redirect();
-    }
-    public function handleProviderFacebookCallback()
-    {
-         $user = Socialite::driver('facebook')->user();
-        // dd($user);
-    //     $fb=new Facebook(config('facebook.config'));
-    //     // $response = $fb->get('/me', $user->token);
-    // $access_token='EAACeZCVjM7usBAE7qcz0JqhLFcNeTIYPo1VeX503bzF0IkzZAxVd1sGSEyduvlXq4t3iMfYIGVVRhFPXZBVabQGj4UcAjLcfnpJ1DuNhl1sJJobaHjFeRxHsguN1fnaFv3Ij7M7vaNPQGZB7licNHwRxZAoER6HeRw8uZBVro2zokilaBabul6c1hSBD6lejDkzZA5qo3bWSWi66jZAQaNuy';
-    //     $fb->setDefaultAccessToken($access_token);
-    //     $fields = "id,cover,name,first_name,last_name,hometown,age_range,birthday,location,likes,posts,friends,link,gender,email,name_format,locale,picture,timezone,updated_time,verified";
-    //     $fb_user = $fb->get('/me?fields='.$fields)->getGraphNode();
-    //     dd($fb_user);
-        $existingUser = User::where('email', $user->getEmail())->first();
-
-        if ($existingUser) {
-            auth()->login($existingUser, true);
-        } else {
-            $newUser                    = new User;
-            $newUser->provider_name     = 'facebook';
-            $newUser->provider_id       = $user->getId();
-            $newUser->name              = $user->getName();
-            $newUser->email             = $user->getEmail();
-            $newUser->email_verified_at = now();
-            $newUser->avatar            = $user->getAvatar();
-            $newUser->save();
-    
-            auth()->login($newUser, true);
-
-
-        
-    }
-    return redirect()->route('/home');
-
     }
     
 
--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://http://127.0.0.1:8000//login/facebook/callback',
+        'redirect' => 'http://localhost:8000/login/facebook/callback',
     ],
 
 ];
