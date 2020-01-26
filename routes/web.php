60d098311723c2134c957ad873d03047149c7b28
Isra <israalfiki@gmail.com>
2020-01-26 16:19:39 +0200

    added navbar

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
 Route::group(['middleware'=>'auth'], function(){
 //  Route::get('/influencers', 'InfluencerController@index')->name('influencers.index');
 // Route::get('/influencers/{influencer}', 'InfluencerController@show');
-=======
-
-Route::get('/influencers', 'InfluencerController@index')->name('influencers.index')->middleware('auth');
->>>>>>> 6144d6e68c0bd6c8c6aa790f2e532bb7384e253a
 Route::get('/influencers/create', 'InfluencerController@create')->name('influencers.create');
 Route::post('/influencers', 'InfluencerController@store')->name('influencers.store');
 
