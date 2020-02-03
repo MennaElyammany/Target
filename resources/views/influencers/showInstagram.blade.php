b2095153517613f31e0d01ef26676803a7534acf
Aya-Farrag <58622631+Aya-Farrag@users.noreply.github.com>
2020-02-03 16:47:55 +0200

    some edit

--- resources/views/influencers/showInstagram.blade.php
+++ resources/views/influencers/showInstagram.blade.php
@@ -5,6 +5,6 @@
 
 @foreach($media_url_list as $media_url_item)
 <div> 
-   <img src="{{$media_url_item->media_url}}" >
+   <img src="{{$media_url_item->media_url}}" width="200px" class="mb-3 ml-5" >
 </div>
 @endforeach
