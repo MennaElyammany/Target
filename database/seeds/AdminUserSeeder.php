67a13ba7d9e813afea04cca933cff484bb191835
mennaelyammany <mennaelyammany@gmail.com>
2020-02-03 13:22:32 +0200

    create admin seeder

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+
+             'name'=>'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+            'role'=>'Admin',
+
+        ]);
+        $user->assignRole('Admin');
+    }
+        
+    }
+
